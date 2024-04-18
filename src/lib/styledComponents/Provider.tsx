be45a01686c032c2d13f87c32838f46c60e9125d
김철우 <chul8747@naver.com>
2024-04-18 11:19:12 +0900

    fix :: 경로 수정

--- src/lib/styledComponents/Provider.tsx
+++ src/lib/styledComponents/Provider.tsx
@@ -2,7 +2,7 @@
 
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '@/style/GlobalStyle';
-import { theme } from '@/style/theme';
+import theme from '@/style/theme';
 import StyledComponentsRegistry from './registry';
 
 export default function ThemeProviders({
